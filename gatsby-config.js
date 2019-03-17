f377f338cec9ef18439bfc36fd92d839a42005f3
Bernard Bado <bado.bernard94@gmail.com>
2019-03-17 19:41:17 +0100

    Updating site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ const path = require("path")
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Bernard Bado`,
+    description: `I am web developer. Using ReactJS to build modern websites.`,
+    author: `@codewithbernard`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
