749e7ba8973bec2b82d4295820b00b591e489af2
Bernard Bado <bado.bernard94@gmail.com>
2019-03-23 16:56:27 +0100

    Adding offline support

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,18 +20,16 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Bernard Bado, ReactJS Web Developer`,
+        short_name: `Bernard Bado`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/assets/original_face.png`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // 'gatsby-plugin-offline',
+    "gatsby-plugin-offline",
     {
       resolve: `gatsby-plugin-alias-imports`,
       options: {
